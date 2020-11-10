6751554352fa6ced986a73b46fc3697531c7fd9e
GeorgeWu <fysidm@gmail.com>
2020-11-10 21:50:47 +0800

    Change ages

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -4,14 +4,14 @@ import { View, Text, StyleSheet, FlatList } from 'react-native';
 const ListScreen = () => {
   const friends = [
     { name: 'Friend #1', age: 30 },
-    { name: 'Friend #2', age: 30 },
-    { name: 'Friend #3', age: 30 },
-    { name: 'Friend #4', age: 30 },
-    { name: 'Friend #5', age: 30 },
-    { name: 'Friend #6', age: 30 },
-    { name: 'Friend #7', age: 30 },
-    { name: 'Friend #8', age: 30 },
-    { name: 'Friend #9', age: 30 },
+    { name: 'Friend #2', age: 31 },
+    { name: 'Friend #3', age: 32 },
+    { name: 'Friend #4', age: 33 },
+    { name: 'Friend #5', age: 34 },
+    { name: 'Friend #6', age: 35 },
+    { name: 'Friend #7', age: 36 },
+    { name: 'Friend #8', age: 37 },
+    { name: 'Friend #9', age: 38 },
   ];
 
   return (
