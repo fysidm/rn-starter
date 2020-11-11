71f39eb48b223d66c67220a07f08ff84b3cd6f1c
GeorgeWu <fysidm@gmail.com>
2020-11-11 17:35:07 +0800

    Add absoluteFillObject

--- src/screens/PositionScreen.js
+++ src/screens/PositionScreen.js
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
   textTwoStyle: {
     borderWidth: 3,
     borderColor: 'red',
-    top: 10
+    ...StyleSheet.absoluteFillObject
   },
   textThreeStyle: {
     borderWidth: 3,
