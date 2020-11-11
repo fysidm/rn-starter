8ad46743ee7fce3968009323c2f893ada9716f3d
GeorgeWu <fysidm@gmail.com>
2020-11-11 17:01:18 +0800

    Add BoxScreen

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,15 @@
+// Demo of Box Object Model
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+  return (
+    <View>
+      <Text>Box Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BoxScreen;
